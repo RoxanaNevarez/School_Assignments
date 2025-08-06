5549dec7c087a407db432590e9f523df4c25f998
Roxana Nevarez <roxana.m.nevarez@gmail.com>
2025-08-05 21:58:56 -0700

    Corrections

--- Intro to C,C++/Ascending or Descending.cpp
+++ Intro to C,C++/Ascending or Descending.cpp
@@ -78,49 +78,44 @@ void descending(int&a, int&b, int&c, int&d)
 int main()
 {
 	int w,x,y,z;
-	char ch_one,a,d,ch_two,Y,N;
-	string word; // this only reads up to the first whitespace of the word
-				 // user can enter an abbreviation or half a word and it will still execute the correct command
+	char ch_one,a,d,ch_two,Y,N; //ch_one corresponds to a,d; ch_two corresponds to Y,N
 	cout << "Enter four integers: \n";
 	cin >> w >> x >> y >> z;
 	cout << "Do you want numbers in ascending [a] or descending [d] order?: ";
 	cin >> ch_one;
-	cin >> word;
 	
-	if (ch_one == 'a' || ch_one == 'A' || ch_one == 'ascending' || ch_one == 'Ascending')
+	if (ch_one == 'a' || ch_one == 'A')
 	{
 		ascending(w,x,y,z);
 		cout << "Ascending Order: " << w << " " << x << " " << y << " " << z;
 	}
-	if (ch_one == 'd' || ch_two == 'D' || ch_one == 'descending' || ch_one == 'Descending')
+	if (ch_one == 'd' || ch_one == 'D')
 	{
 		descending(w,x,y,z);
 		cout << "Descending Order: " << w << " " << x << " " << y << " " << z;
 	}
 	cout << "\nWould you like to try a different set of integers? Yes [Y] | No [N]: ";
 	cin >> ch_two;
-	cin >> word;
 	
-	while (ch_two == 'Y' || ch_two == 'y' || ch_two == 'Yes' || ch_two == 'yes')
+	while (ch_two == 'Y' || ch_two == 'y')
 	{
 		cout << "\nEnter four integers: \n";
 	cin >> w >> x >> y >> z;
 	cout << "Do you want numbers in ascending [a] or descending [d] order?: ";
 	cin >> ch_one;
-	cin >> word;
-	if (ch_one == 'a' || ch_one == 'A' || ch_one == 'ascending' || ch_one == 'Ascending')
+
+	if (ch_one == 'a' || ch_one == 'A')
 	{
 		ascending(w,x,y,z);
 		cout << "Ascending Order: " << w << " " << x << " " << y << " " << z;
 	}
-	if (ch_one == 'd' || ch_two == 'D' || ch_one == 'descending' || ch_one == 'Descending')
+	if (ch_one == 'd' || ch_one == 'D')
 	{
 		descending(w,x,y,z);
 		cout << "Descending Order: " << w << " " << x << " " << y << " " << z;
 	}
 	cout << "\nWould you like to try a different set of integers? Yes [Y] | No [N]: ";
 	cin >> ch_two;
-	cin >> word;
 	}
 	
 	return 0;
